63b3edbb950c301041a87064d3ef5693e755bd80
Michael Quigley <mikequigley@quigleypublishing.com>
2020-02-07 19:24:07 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -41,33 +41,33 @@ def consolidate_cart(cart)
 end
 
 def apply_coupons(cart, coupons)
-  
-  # Using the logic and variables from the video walkthrough now, 
+
+  # Using the logic and variables from the video walkthrough now,
   # except using 'i' for the counter
   # (my original attempt got too convoluted - starting over)
-  i = 0 
-  while i < coupons.length 
+  i = 0
+  while i < coupons.length
     cart_item = find_item_by_name_in_collection(coupons[i][:item], cart)
-    couponed_item_name = "#{coupons[i][:item]} W/COUPON" 
+    couponed_item_name = "#{coupons[i][:item]} W/COUPON"
     cart_item_with_coupon = find_item_by_name_in_collection(couponed_item_name, cart)
-    if cart_item && cart_item[:count] >= coupons[i][:num] 
-      if cart_item_with_coupon 
-        cart_item_with_coupon[:count] += coupons[i][:num] 
-        cart_item[:count] -= coupons[counter][:num] 
-      else 
+    if cart_item && cart_item[:count] >= coupons[i][:num]
+      if cart_item_with_coupon
+        cart_item_with_coupon[:count] += coupons[i][:num]
+        cart_item[:count] -= coupons[counter][:num]
+      else
         cart_item_with_coupon = {
-          :item => couponed_item_name,  
-          :price => coupons[i][:cost] / coupons[i][:num], 
-          :count => coupons[i][:num], 
+          :item => couponed_item_name,
+          :price => coupons[i][:cost] / coupons[i][:num],
+          :count => coupons[i][:num],
           :clearance => cart_item[:clearance]
         }
         cart << cart_item_with_coupon
         cart_item[:count] -= coupons[i][:num]
-      end 
-    end 
-    counter += 1 
-  end 
-  cart 
+      end
+    end
+    counter += 1
+  end
+  cart
 end
 
 def apply_clearance(cart)
